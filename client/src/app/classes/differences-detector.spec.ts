74714f8103603776205950fdb6904c233beb3701
Charles-Antoine23 <charles-antoine.vezina@polymtl.ca>
2022-09-14 18:21:52 -0400

    Ajout de la librairie Rembrandt

---
+++ client/src/app/classes/differences-detector.spec.ts
@@ -0,0 +1,7 @@
+import { DifferencesDetector } from './differences-detector';
+
+describe('DifferencesDetector', () => {
+  it('should create an instance', () => {
+    expect(new DifferencesDetector()).toBeTruthy();
+  });
+});
