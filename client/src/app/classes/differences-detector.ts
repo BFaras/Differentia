982edc3afe4d72e2f4c8c7bc35a94c56ebbb40ba
Charles-Antoine23 <charles-antoine.vezina@polymtl.ca>
2022-09-15 15:58:48 -0400

    Ajout des methodes pour le systeme de difference

--- client/src/app/classes/differences-detector.ts
+++ client/src/app/classes/differences-detector.ts
@@ -1,41 +1,55 @@
-import { Image } from 'canvas';
+import { Canvas, Image } from 'canvas';
+import { CanvasToCompare } from './canvas-to-compare';
+import { ImagesToCompare } from './images-to-compare';
+
+const NB_BIT_PER_PIXEL = 4;
+const RED_POS = 0;
+const GREEN_POS = 1;
+const BLUE_POS = 2;
+const ALPHA_POS = 3;
+
+const BLACK = 0;
+const ALPHA_OPAQUE = 1;
 
 export class DifferencesDetector {
-    originalImage: Image;
-    modifiedImage: Image;
-    radius: number;
+    private whiteImageData;
 
-    constructor(originalImage: Image, modifiedImage: Image, radius: number) {
-        this.originalImage = originalImage;
-        this.modifiedImage = modifiedImage;
-        this.radius = radius;
+    constructor(readonly imagesToCompare: ImagesToCompare, readonly canvasToCompare: CanvasToCompare, readonly radius: number) {
+        // Ajouter la verification de grandeur
+        const whiteCanvas = new Canvas(this.imagesToCompare.originalImage.width, this.imagesToCompare.originalImage.height);
+        const whiteImageContext = whiteCanvas.getContext('2d');
+        whiteImageContext.fillStyle = 'white';
+        whiteImageContext.fillRect(0, 0, whiteCanvas.width, whiteCanvas.height);
+
+        this.whiteImageData = whiteImageContext.getImageData(0, 0, whiteCanvas.width, whiteCanvas.height);
     }
 
-    getDiffercences() {
-        const originalImageCanvas = document.createElement('canvas').getContext('2d');
-        const modifiedImageCanvas = document.createElement('canvas').getContext('2d');
+    getImageData(image: Image, canvas: Canvas) {
+        const imageContext = canvas.getContext('2d');
+        imageContext.drawImage(image, 0, 0);
+        return imageContext.getImageData(0, 0, canvas.width, canvas.height).data;
+    }
 
-        originalImageCanvas.canvas.width = this.originalImage.width;
-        modifiedImageCanvas8.canvas.width = this.modifiedImage.width;
+    getDifferencesTest(originalImage: Image, modifiedImage: Image) {
+        const originalImageData = this.getImageData(originalImage, this.canvasToCompare.originalImageCanvas);
+        const modifiedImageData = this.getImageData(modifiedImage, this.canvasToCompare.modifiedImageCanvas);
 
-        originalImageCanvas.drawImage(this.originalImage, 0, 0);
-        modifiedImageCanvas.drawImage(this.modifiedImage, 0, 0);
+        for (let i = 0; i < originalImageData.length; i += NB_BIT_PER_PIXEL) {
+            const redDiff = originalImageData[i + RED_POS] - modifiedImageData[i + RED_POS];
+            const greenDiff = originalImageData[i + GREEN_POS] - modifiedImageData[i + GREEN_POS];
+            const blueDiff = originalImageData[i + BLUE_POS] - modifiedImageData[i + BLUE_POS];
+            const alphaDiff = originalImageData[i + ALPHA_POS] - modifiedImageData[i + ALPHA_POS];
 
-        const dataOriginal = originalImageCanvas.getImageData(0, 0, this.originalImage.width, this.originalImage.height);
-        const dataModified = modifiedImageCanvas.getImageData(0, 0, this.modifiedImage.width, this.modifiedImage.height);
-
-        const different = [];
-
-        const NB_PIXELS = 4;
-        for (let y = 0; y < this.originalImage.height; y++) {
-            for (let x = 0; x < this.originalImage.width; x++) {
-                const pos = x * NB_PIXELS + y * (this.originalImage.width * NB_PIXELS);
-                for (let i = 0; i < NB_PIXELS; i++) {
-                    if (dataOriginal[pos + i] !== dataModified[pos + 1]) {
-                        different.push({ x, y });
-                    }
-                }
+            if (redDiff !== 0 || greenDiff !== 0 || blueDiff !== 0 || alphaDiff !== 0) {
+                this.generateDiffImage(i);
             }
         }
+    }
+
+    generateDiffImage(pixelPosition: number) {
+        this.whiteImageData.data[pixelPosition + RED_POS] = BLACK;
+        this.whiteImageData.data[pixelPosition + GREEN_POS] = BLACK;
+        this.whiteImageData.data[pixelPosition + BLUE_POS] = BLACK;
+        this.whiteImageData.data[pixelPosition + ALPHA_POS] = ALPHA_OPAQUE;
     }
 }
