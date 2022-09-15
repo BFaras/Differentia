e243fa4635694f1579f1c8d1cc70a50bebc4e4f5
BFaras <71102846+BFaras@users.noreply.github.com>
2022-09-15 16:03:20 -0400

    Creation de la vue, modification du Css

---
+++ client/src/app/components/editable-images/editable-images.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editable-images',
+  templateUrl: './editable-images.component.html',
+  styleUrls: ['./editable-images.component.scss']
+})
+export class EditableImagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameCreationPageComponent } from '@app/pages/game-creation-page/game-creation-page.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
-import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 
 const routes: Routes = [
-    //on va mettre page ici
+    // on va mettre page ici
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
-    { path: 'material', component: MaterialPageComponent },
+    { path: 'gameCreation', component: GameCreationPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
