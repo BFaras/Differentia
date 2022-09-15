b31973cff0c69bfb58cb411370027a3ad48665ca
Charles-Antoine23 <charles-antoine.vezina@polymtl.ca>
2022-09-15 11:45:00 -0400

    Debut de la classe et du constructeur DifferencesDetector

--- client/src/app/classes/differences-detector.ts
+++ client/src/app/classes/differences-detector.ts
@@ -1,2 +1,13 @@
+import { Image } from 'canvas';
+
 export class DifferencesDetector {
+    originalImage: Image;
+    modifiedImage: Image;
+    radius: number;
+
+    constructor(originalImage: Image, modifiedImage: Image, radius: number) {
+        this.originalImage = originalImage;
+        this.modifiedImage = modifiedImage;
+        this.radius = radius;
+    }
 }
