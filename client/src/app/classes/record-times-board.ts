42b7435848358ed9b9e99f532c1a5ca4038313cf
rak553 <raphael62243@gmail.com>
2022-09-17 15:45:46 -0400

    RL Correction d'envoi de la definition par defaut de RecordTimesBoard dans formService et RecordTimesBoard

--- client/src/app/classes/record-times-board.ts
+++ client/src/app/classes/record-times-board.ts
@@ -5,7 +5,12 @@ export class RecordTimesBoard {
     public recordTimesVersus: RecordTime[];
 
     constructor(soloRecord: RecordTime[], versusRecords: RecordTime[]) {
-        this.recordTimesSolo = [new RecordTime('02:00', 'Mark'), new RecordTime('02:00', 'Jean'), new RecordTime('02:00', 'Paul')];
-        this.recordTimesVersus = [new RecordTime('02:00', 'Brook'), new RecordTime('02:00', 'Leon'), new RecordTime('02:00', 'Mike')];
+        if (soloRecord.length == 0) {
+            this.recordTimesSolo = [new RecordTime('02:00', 'Mark'), new RecordTime('02:00', 'Jean'), new RecordTime('02:00', 'Paul')];
+        }
+
+        if (versusRecords.length == 0) {
+            this.recordTimesVersus = [new RecordTime('02:00', 'Brook'), new RecordTime('02:00', 'Leon'), new RecordTime('02:00', 'Mike')];
+        }
     }
 }
