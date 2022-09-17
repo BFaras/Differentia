c08445e536d5d69c82a42a7697fd0a11c5aee5d4
rak553 <raphael62243@gmail.com>
2022-09-17 15:03:48 -0400

    Tests pour GameFormComponent

--- client/src/app/components/forms/game-form/game-form.component.spec.ts
+++ client/src/app/components/forms/game-form/game-form.component.spec.ts
@@ -1,23 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { GameFormDescription } from '../../../classes/game-form-description';
+import { RecordTimesBoard } from '../../../classes/record-times-board';
 import { GameFormComponent } from './game-form.component';
 
 describe('GameFormComponent', () => {
-  let component: GameFormComponent;
-  let fixture: ComponentFixture<GameFormComponent>;
+    const gameName = 'name';
+    const gameImage = 'image';
+    const recordTimesBoard = new RecordTimesBoard([], []);
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ GameFormComponent ]
-    })
-    .compileComponents();
+    let gameFormComp: GameFormComponent;
+    let fixture: ComponentFixture<GameFormComponent>;
 
-    fixture = TestBed.createComponent(GameFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [GameFormComponent],
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(GameFormComponent);
+        gameFormComp = fixture.componentInstance;
+        gameFormComp.gameForm = new GameFormDescription(gameName, gameImage, recordTimesBoard);
+        fixture.detectChanges();
+    });
+
+    it('should create component', () => {
+        expect(gameFormComp).toBeTruthy();
+    });
+
+    it('GameFormDescription should be the one that was in the Input', () => {
+        expect(gameFormComp.gameForm.gameName).toEqual(gameName);
+        expect(gameFormComp.gameForm.gameImage).toEqual(gameImage);
+        expect(gameFormComp.gameForm.recordTimesBoard).toEqual(recordTimesBoard);
+    });
 });
