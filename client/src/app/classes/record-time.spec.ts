bbfaf97484a8f4569ff5d75157f6f026352c81a7
rak553 <raphael62243@gmail.com>
2022-09-17 15:54:59 -0400

    RL Ajout des tests pour recordTime

--- client/src/app/classes/record-time.spec.ts
+++ client/src/app/classes/record-time.spec.ts
@@ -1,5 +1,20 @@
-// describe('RecordTime', () => {
-//   it('should create an instance', () => {
-//     expect(new RecordTime()).toBeTruthy();
-//   });
-// });
+import { RecordTime } from '@app/classes/record-time';
+
+describe('RecordTime', () => {
+    const plrName = 'hisName';
+    const plrTime = '1:00';
+    let recordTime: RecordTime;
+
+    beforeEach(() => {
+        recordTime = new RecordTime(plrTime, plrName);
+    });
+
+    it('should create an instance', () => {
+        expect(recordTime).toBeTruthy();
+    });
+
+    it('instance should have correct values instantiated', () => {
+        expect(recordTime.playerName).toEqual(plrName);
+        expect(recordTime.time).toEqual(plrTime);
+    });
+});
