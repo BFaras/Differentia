7493b1706e248f4578b59a1edc8f5b526ca0660f
David Amoussa <henoc-jean-david.amoussa@polymtl.ca>
2022-09-19 15:34:14 -0400

    Ajout des tests

--- client/src/app/components/dialog-input/dialog-input.component.spec.ts
+++ client/src/app/components/dialog-input/dialog-input.component.spec.ts
@@ -1,4 +1,8 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { DialogInputComponent } from './dialog-input.component';
 
@@ -9,6 +13,12 @@ describe('DialogInputComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [DialogInputComponent],
+            schemas: [NO_ERRORS_SCHEMA],
+            imports: [MatFormFieldModule, MatInputModule, MatDialogModule],
+            providers: [
+                { provide: MatDialogRef, useValue: {} },
+                { provide: MAT_DIALOG_DATA, useValue: {} },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(DialogInputComponent);
