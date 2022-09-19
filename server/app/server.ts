c92d14617ffeae29e191116d0a41650363591e82
sebastian-villanue <71283391+sebastian-villanue@users.noreply.github.com>
2022-09-19 12:07:05 -0400

    SV Création du service de la base données

--- server/app/server.ts
+++ server/app/server.ts
@@ -1,7 +1,10 @@
 import { Application } from '@app/app';
+import 'dotenv/config';
 import * as http from 'http';
 import { AddressInfo } from 'net';
 import { Service } from 'typedi';
+import { DatabaseService } from './services/database.service';
+
 
 @Service()
 export class Server {
@@ -10,7 +13,10 @@ export class Server {
     private static readonly baseDix: number = 10;
     private server: http.Server;
 
-    constructor(private readonly application: Application) {}
+    constructor(
+        private readonly application: Application,
+        private databaseService: DatabaseService
+    ) {}
 
     private static normalizePort(val: number | string): number | string | boolean {
         const port: number = typeof val === 'string' ? parseInt(val, this.baseDix) : val;
@@ -22,7 +28,7 @@ export class Server {
             return false;
         }
     }
-    init(): void {
+    async init(): Promise<void> {
         this.application.app.set('port', Server.appPort);
 
         this.server = http.createServer(this.application.app);
@@ -30,6 +36,13 @@ export class Server {
         this.server.listen(Server.appPort);
         this.server.on('error', (error: NodeJS.ErrnoException) => this.onError(error));
         this.server.on('listening', () => this.onListening());
+        try {
+            await this.databaseService.start();
+            console.log("Database connection successful !");
+          } catch {
+            console.error("Database connection failed !");
+            process.exit(1);
+          }
     }
 
     private onError(error: NodeJS.ErrnoException): void {
---
+++ server/app/services/games.service.ts
@@ -0,0 +1,149 @@
+import { Game } from '@app/classes/game';
+import { Time } from '../../../common/time'
+import { Collection, Filter, FindOptions, ModifyResult, UpdateFilter, WithId } from 'mongodb';
+import { HttpException } from '@app/classes/http.exception';
+import { DatabaseService } from './database.service';
+import { Service } from 'typedi';
+import 'dotenv/config';
+
+// J'ai recherché les jeux par leurs noms, est-ce c'est correct que je le fasse à partir de cet attribut????
+// Est-ce que je devrais transformer toutes les throw new Error en throw new httpException?????
+
+// const OK_CODE: number = 200;
+// const CREATED_CODE: number = 201;
+// const NO_CONTENT_CODE: number = 204;
+// const NOT_MODIFIED_CODE: number = 304;
+// const BAD_REQUEST_CODE: number = 400;
+// const FORBIDDEN_CODE: number = 403;
+const NOT_FOUND_CODE: number = 404;
+// const GONE_CODE: number = 410;
+const INTERNAL_SERVER_ERROR_CODE: number = 500;
+// const NOT_IMPLEMENTED_CODE: number = 501;
+
+@Service()
+export class GamesService {
+
+  constructor(private databaseService: DatabaseService) {
+  }
+
+  get collection(): Collection<Game> {
+    return this.databaseService.database.collection(
+        process.env.DATABASE_COLLECTION!
+    );
+  }
+
+  async getAllGames(): Promise<Game[]> {
+    return this.collection
+      .find({})
+      .toArray()
+      .then((game: Game[]) => {
+        return game;
+      });
+  }
+
+  async getGame(nameOfWantedGame: string): Promise<Game> {
+    // NB: This can return null if the course does not exist, you need to handle it
+    return this.collection
+      .findOne({ name: nameOfWantedGame })
+      .then((game: WithId<Game>) => {
+        if(game) {
+            return game;
+        }
+        throw new HttpException('Game not found', NOT_FOUND_CODE);
+      });
+  }
+
+  async addGame(game: Game): Promise<void> {
+    if (this.validateGame(game)) {
+      await this.collection.insertOne(game).catch((error: Error) => {
+        throw new HttpException('Failed to add game', INTERNAL_SERVER_ERROR_CODE);
+      });
+    } else {
+      throw new Error('Invalid game');
+    }
+  }
+
+  async deleteGame(nameOfWantedGame: string): Promise<void> {
+    return this.collection
+      .findOneAndDelete({ name: nameOfWantedGame })
+      .then((res: ModifyResult<Game>) => {
+        if (!res.value) {
+          throw new HttpException('Could not find game', NOT_FOUND_CODE);
+        }
+      })
+      .catch(() => {
+        throw new HttpException('Failed to delete game', INTERNAL_SERVER_ERROR_CODE);
+      });
+  }
+
+  async modifyGame(game: Game): Promise<void> {
+    let filterQuery: Filter<Game> = { name: game.name };
+    let updateQuery: UpdateFilter<Game> = {
+      $set: {
+        name: game.name,
+        numberOfDifferences: game.numberOfDifferences,
+        times: game.times,
+        images: game.images, 
+      },
+    };
+    // Can also use replaceOne if we want to replace the entire object
+    return this.collection
+      .updateOne(filterQuery, updateQuery)
+      .then(() => { })
+      .catch(() => {
+        throw new Error('Failed to update game');
+      });
+  }
+
+  async getGameTimes(nameOfWantedGame: string): Promise<Time[]> {
+    let filterQuery: Filter<Game> = { name: nameOfWantedGame };
+    // Only get the times and not any of the other fields
+    let projection: FindOptions = { projection: { times: 1, _id: 0 } };
+    return this.collection
+      .findOne(filterQuery, projection)
+      .then((game: WithId<Game>) => {
+        return game.times;
+      })
+      .catch(() => {
+        throw new Error('Failed to get the game times');
+      });
+  }
+
+  async getGameName(nameOfWantedGame: string): Promise<string> {
+    let filterQuery: Filter<Game> = { name: nameOfWantedGame };
+    return this.collection
+      .findOne(filterQuery)
+      .then((game: WithId<Game>) => {
+        return game.name;
+      });
+  }
+
+  async getGameNumberOfDifferences(nameOfWantedGame: string): Promise<number> {
+    let filterQuery: Filter<Game> = { name: nameOfWantedGame };
+    return this.collection
+      .findOne(filterQuery)
+      .then((game: WithId<Game>) => {
+        return game.numberOfDifferences;
+      });
+  }
+
+  private validateGame(game: Game): boolean {
+    return (
+      this.validateNumberOfDifferences(game.numberOfDifferences) &&
+      this.validateImages(game.images) && // est-ce qu'on devrait regarder si le jeu a tous ses attributs initialisés? ou c'est de l'abus
+      this.validateTimes(game.times)
+    );
+  }
+
+  private validateNumberOfDifferences(numberOfDifferences: number): boolean {
+    return numberOfDifferences >= 3 && numberOfDifferences <= 9;
+  }
+
+  private validateImages(images: string[]): boolean { // À revoir l'implémentation de cette fonction
+    return images[0] != null && images[1] != null;
+  }
+
+  private validateTimes(times: Time[]): boolean {
+    return times.length === 0;
+  }
+}
