7ba4e948eff0b933bf6e659427d5d0c312a6c910
rak553 <raphael62243@gmail.com>
2022-09-22 12:59:57 -0400

    Debut tests

--- client/src/app/pages/test-page/test-page.component.ts
+++ client/src/app/pages/test-page/test-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Image } from 'canvas';
 
 @Component({
   selector: 'app-test-page',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-page.component.scss']
 })
 export class TestPageComponent implements OnInit {
+  const originalImage: Image ;
+  const diffImage: Image;
+  let finalDifferencesImage: Image;
 
   constructor() { }
 
