1267a81dcf89e365531d2cc16ea3cc3fb9ffa702
David Amoussa <henoc-jean-david.amoussa@polymtl.ca>
2022-09-25 17:21:52 -0400

    Ajout de méthodes pour les cliques

--- client/src/app/components/play-area/play-area.component.ts
+++ client/src/app/components/play-area/play-area.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MouseButton } from '@app/interfaces/mouseButton';
+import { Vec2 } from '@app/interfaces/vec2';
+import { DrawService } from '@app/services/draw.service';
+import { DEFAULT_HEIGHT_CANVAS, DEFAULT_WIDTH_CANVAs } from '@common/const';
 
 @Component({
     selector: 'app-play-area',
     templateUrl: './play-area.component.html',
     styleUrls: ['./play-area.component.scss'],
 })
-export class PlayAreaComponent {}
+
+export class PlayAreaComponent {
+    @ViewChild('gridCanvas', { static: false }) private canvas!: ElementRef<HTMLCanvasElement>;
+
+    mousePosition: Vec2 = { x: 0, y: 0 };
+
+    private canvasSize = { x: DEFAULT_WIDTH_CANVAs, y: DEFAULT_HEIGHT_CANVAS };
+    constructor(private readonly drawService: DrawService) {}
+
+    get width(): number {
+        return this.canvasSize.x;
+    }
+
+    get height(): number {
+        return this.canvasSize.y;
+    }
+
+    ngAfterViewInit(): void {
+        this.drawService.context = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
+        this.canvas.nativeElement.focus();
+    }
+
+    // TODO : déplacer ceci dans un service de gestion de la souris!
+    mouseHitDetect(event: MouseEvent) {
+        if (event.button === MouseButton.Left) {
+            this.mousePosition = { x: event.offsetX, y: event.offsetY };
+        }
+    }
+}
