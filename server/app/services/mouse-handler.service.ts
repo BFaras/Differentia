ec1af62dfc5f894e3b8848048a47bf5d6695435c
IsmaelAs <50330186+IsmaelAs@users.noreply.github.com>
2022-09-26 23:51:06 -0400

    Sauvegarder et Loader HashMap de diff dans le JSON

--- server/app/services/mouse-handler.service.ts
+++ server/app/services/mouse-handler.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { MouseButton } from '@app/interfaces/mouseButton';
 import { Vec2 } from '@app/interfaces/vec2';
-import {DifferencesDetector} from '../diff'
+import {DifferencesDetector} from '@common/differences-classes/differences-detector'
+
 @Injectable({
   providedIn: 'root'
 })
 export class MouseHandlerService {
 
   mousePosition: Vec2;
+  differencesHashmap: Map <number, number>;
 
   constructor() {
     this.mousePosition = { x: 0, y: 0 };
+    this.differencesHashmap = new Map<number, number>();
   }
 
   mouseHitDetect(event: MouseEvent) {
@@ -19,9 +22,17 @@ export class MouseHandlerService {
     }
   }
 
-  saveHashMapInJson() {
+  // Sauvegarder la hashmap de diff dans le games.json
+  // Par contre la méthode devrait être ici ou dans diff-detector ??
+  saveHashMapInJson(){
 
   }
+
+  loadHashMapFromJson() {
+
+  }
+
+  con
   
   
 }
