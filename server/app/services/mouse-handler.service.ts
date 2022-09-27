4f6b20ee90885c3f96baf1ff77baf4f507629511
IsmaelAs <50330186+IsmaelAs@users.noreply.github.com>
2022-09-26 20:56:17 -0400

    Partager accès client-serveur à DiffDetector

--- server/app/services/mouse-handler.service.ts
+++ server/app/services/mouse-handler.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@angular/core';
 import { MouseButton } from '@app/interfaces/mouseButton';
 import { Vec2 } from '@app/interfaces/vec2';
-
+import {DifferencesDetector} from '../diff'
 @Injectable({
   providedIn: 'root'
 })
 export class MouseHandlerService {
 
-  mousePosition: Vec2 = { x: 0, y: 0 };
+  mousePosition: Vec2;
 
-  constructor() { }
+  constructor() {
+    this.mousePosition = { x: 0, y: 0 };
+  }
 
   mouseHitDetect(event: MouseEvent) {
     if (event.button === MouseButton.Left) {
         this.mousePosition = { x: event.offsetX, y: event.offsetY };
     }
   }
+
+  saveHashMapInJson() {
+
+  }
+  
+  
 }
