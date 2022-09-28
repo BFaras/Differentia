8b47ad1d7158198bbed7c5bf3008627c1af5b42e
Charles-Antoine23 <charles-antoine.vezina@polymtl.ca>
2022-09-28 12:26:00 -0400

    Ajout de test pour le sercice d'image difference

--- client/src/app/services/image-to-image-difference.service.spec.ts
+++ client/src/app/services/image-to-image-difference.service.spec.ts
@@ -1,16 +1,42 @@
-import { TestBed } from '@angular/core/testing';
+import { Renderer2 } from '@angular/core';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ImageToImageDifferenceService } from './image-to-image-difference.service';
 
 describe('ImageToImageDifferenceService', () => {
-  let service: ImageToImageDifferenceService;
+    let service: ImageToImageDifferenceService;
+    let renderer: Renderer2;
+    let mainCanvas: HTMLCanvasElement;
+    let originalImage: HTMLImageElement = new Image();
+    let modifiedImage: HTMLImageElement = new Image();
+    let differencesImageToPutDataIn: HTMLImageElement;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ImageToImageDifferenceService);
-  });
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({ declarations: [ImageToImageDifferenceService] }).compileComponents();
+    }));
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        mainCanvas = renderer.createElement('canvas');
+        service = TestBed.inject(ImageToImageDifferenceService);
+        originalImage.src = '../../assets/ImageBlanche.bmp';
+        modifiedImage.src = '../../assets/image_7_diff.bmp';
+    });
+
+    // Pas trop certain de l'utilite
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should call setupDataInService with the right attribute', () => {
+        // service.setupDataInService()
+    });
+
+    it('should change the size of the canvas accordingly', () => {
+        // service.adaptCanvasSizeToImage
+    });
+
+    it('should return the right image data', () => {
+        // service.getImageData
+    });
 });
