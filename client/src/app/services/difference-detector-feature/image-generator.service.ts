c43daa8784f0c4cbe35dbe20f23bc70e562f269b
rak553 <raphael62243@gmail.com>
2022-10-01 11:25:04 -0400

    RL Refactoring the imageGeneratorService

--- client/src/app/services/difference-detector-feature/image-generator.service.ts
+++ client/src/app/services/difference-detector-feature/image-generator.service.ts
@@ -5,30 +5,29 @@ import { ALPHA_OPAQUE, ALPHA_POS, BLACK_RGB, BLUE_POS, GREEN_POS, NB_BIT_PER_PIX
     providedIn: 'root',
 })
 export class ImageGeneratorService {
-    private generatedImageData;
+    constructor() {}
 
-    constructor(private readonly canvasToDrawOn: HTMLCanvasElement) {
-        const whiteImageContext: CanvasRenderingContext2D = this.canvasToDrawOn.getContext('2d')!;
-        whiteImageContext!.fillStyle = 'white';
-        whiteImageContext!.fillRect(0, 0, this.canvasToDrawOn.width, this.canvasToDrawOn.height);
+    generateImageFromPixelsDataArray(differentPixelsArray: number[], canvasToDrawOn: HTMLCanvasElement): ImageData {
+        this.setupCanvas(canvasToDrawOn);
+        const generatedImageData = canvasToDrawOn.getContext('2d')!.getImageData(0, 0, canvasToDrawOn.width, canvasToDrawOn.height);
 
-        this.generatedImageData = whiteImageContext.getImageData(0, 0, this.canvasToDrawOn.width, this.canvasToDrawOn.height);
-    }
-
-    getGeneratedImageData(): ImageData {
-        return this.generatedImageData;
-    }
-
-    generateImageFromPixelsDataArray(differentPixelsArray: number[]) {
         for (let i = 0; i < differentPixelsArray.length; i++) {
-            this.generateBlackPixel(differentPixelsArray[i] * NB_BIT_PER_PIXEL);
+            this.generateBlackPixel(differentPixelsArray[i] * NB_BIT_PER_PIXEL, generatedImageData);
         }
+
+        return generatedImageData;
     }
 
-    private generateBlackPixel(pixelPositionInDataArray: number) {
-        this.generatedImageData.data[pixelPositionInDataArray + RED_POS] = BLACK_RGB;
-        this.generatedImageData.data[pixelPositionInDataArray + GREEN_POS] = BLACK_RGB;
-        this.generatedImageData.data[pixelPositionInDataArray + BLUE_POS] = BLACK_RGB;
-        this.generatedImageData.data[pixelPositionInDataArray + ALPHA_POS] = ALPHA_OPAQUE;
+    private setupCanvas(canvasToDrawOn: HTMLCanvasElement) {
+        const whiteImageContext: CanvasRenderingContext2D = canvasToDrawOn.getContext('2d')!;
+        whiteImageContext!.fillStyle = 'white';
+        whiteImageContext!.fillRect(0, 0, canvasToDrawOn.width, canvasToDrawOn.height);
+    }
+
+    private generateBlackPixel(pixelPositionInDataArray: number, generatedImageData: ImageData) {
+        generatedImageData.data[pixelPositionInDataArray + RED_POS] = BLACK_RGB;
+        generatedImageData.data[pixelPositionInDataArray + GREEN_POS] = BLACK_RGB;
+        generatedImageData.data[pixelPositionInDataArray + BLUE_POS] = BLACK_RGB;
+        generatedImageData.data[pixelPositionInDataArray + ALPHA_POS] = ALPHA_OPAQUE;
     }
 }
