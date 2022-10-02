34957cd07eaf1713536b45fad924abe445da6704
Charles-Antoine23 <charles-antoine.vezina@polymtl.ca>
2022-10-01 23:03:07 -0400

    Affichage d'une image dans gameSolo

--- client/src/app/components/play-area/play-area.component.ts
+++ client/src/app/components/play-area/play-area.component.ts
@@ -1,10 +1,10 @@
-import { Component, ElementRef, OnInit, ViewChild, Renderer2 } from '@angular/core';
-import { Position } from '@common/position';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { DrawService } from '@app/services/draw.service';
-import { DEFAULT_HEIGHT_CANVAS, DEFAULT_WIDTH_CANVAs } from '@common/const';
+import { ImageToImageDifferenceService } from '@app/services/image-to-image-difference.service';
 import { MouseDetectionService } from '@app/services/mouse-detection.service';
 import { SocketClientService } from '@app/services/socket-client.service';
-import { ImageToImageDifferenceService } from '@app/services/image-to-image-difference.service';
+import { DEFAULT_HEIGHT_CANVAS, DEFAULT_WIDTH_CANVAs } from '@common/const';
+import { Position } from '@common/position';
 @Component({
     selector: 'app-play-area',
     templateUrl: './play-area.component.html',
@@ -18,11 +18,13 @@ export class PlayAreaComponent implements OnInit {
     readonly finalDifferencesImage: HTMLImageElement = new Image();
 
     private canvasSize = { x: DEFAULT_WIDTH_CANVAs, y: DEFAULT_HEIGHT_CANVAS };
-    constructor(public socketService: SocketClientService,
+    constructor(
+        public socketService: SocketClientService,
         private readonly drawService: DrawService,
         private readonly mouseDetection: MouseDetectionService,
         private renderer: Renderer2,
-        private imageToImageDifferenceService: ImageToImageDifferenceService) {}
+        private imageToImageDifferenceService: ImageToImageDifferenceService,
+    ) {}
 
     async ngOnInit(): Promise<void> {
         this.socketService.connect();
@@ -30,9 +32,9 @@ export class PlayAreaComponent implements OnInit {
 
         const mainCanvas = this.renderer.createElement('canvas');
 
-        this.originalImage.src = '../../../assets/ImageBlanche.bmp';
+        this.originalImage.src = '../../../assets/images/image_7_diff.bmp';
         await this.imageToImageDifferenceService.waitForImageToLoad(this.originalImage);
-        this.modifiedImage.src = '../../../assets/image_7_diff.bmp';
+        this.modifiedImage.src = '../../../assets/images/image_7_diff.bmp';
         await this.imageToImageDifferenceService.waitForImageToLoad(this.modifiedImage);
 
         this.imageToImageDifferenceService.sendDifferentImagesInformationToServerForGameSolo(
@@ -54,20 +56,21 @@ export class PlayAreaComponent implements OnInit {
 
     ngAfterViewInit(): void {
         this.drawService.context = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
+        this.drawService.context.drawImage(this.originalImage, 0, 0);
         this.canvas.nativeElement.focus();
     }
 
-   /* ngOnInit() {
+    /* ngOnInit() {
         this.socketService.connect();
         this.configurePlayAreaSocket();
     }*/
 
-    detectDifference(event: MouseEvent){
+    detectDifference(event: MouseEvent) {
         this.mouseDetection.mouseHitDetect(event);
     }
 
     configurePlayAreaSocket() {
-        this.socketService.on("Valid click",(clickResponse:boolean)=>{
+        this.socketService.on('Valid click', (clickResponse: boolean) => {
             this.mouseDetection.playSound(clickResponse);
             this.mouseDetection.clickMessage(clickResponse);
         });
