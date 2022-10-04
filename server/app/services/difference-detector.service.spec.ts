b6d852a96ff649bd65acf457ee68f60318bbaf54
IsmaelAs <50330186+IsmaelAs@users.noreply.github.com>
2022-10-04 19:12:10 -0400

    Test resetData pour Mousehandler fait

--- server/app/services/difference-detector.service.spec.ts
+++ server/app/services/difference-detector.service.spec.ts
@@ -1,9 +1,20 @@
 import { expect } from 'chai';
-
+import { ImageDataToCompare } from '@common/image-data-to-compare';
 import { DifferenceDetectorService } from './difference-detector.service';
 
 describe('DifferenceDetectorService', () => {
     let service: DifferenceDetectorService;
+    let imagesData: ImageDataToCompare = {
+        originalImageData: new Uint8ClampedArray,
+        modifiedImageData: new Uint8ClampedArray,
+        imageWidth: 1,
+        imageHeight: 1,
+        offSet: 0,
+    };
+
+    beforeEach(async () => {
+        service = new DifferenceDetectorService(imagesData);
+    });
 
     it('should be created', () => {
         expect(service).to.be.true;
--- server/app/services/mouse-handler.service.spec.ts
+++ server/app/services/mouse-handler.service.spec.ts
@@ -6,15 +6,15 @@ import sinon = require('sinon');
 
 describe('MouseHandlerService', () => {
     let mouseService: MouseHandlerService;
-    let board = (1)['board'];
     let position: Position = { x: 0, y: 0 };
     let imageService: ImageDataToCompare = {
-        originalImageData: new Uint8ClampedArray(board),
-        modifiedImageData: new Uint8ClampedArray(board),
+        originalImageData: new Uint8ClampedArray,
+        modifiedImageData: new Uint8ClampedArray,
         imageWidth: 1,
         imageHeight: 1,
-        offSet: 1,
+        offSet: 0,
     };
+
 
     beforeEach(async () => {
         mouseService = new MouseHandlerService();
@@ -39,4 +39,13 @@ describe('MouseHandlerService', () => {
         mouseService.isValidClick(position);
         sinon.assert.alwaysCalledWith(convertPositionspy, position);
     });
+
+    it('should reset differencesHashmap and differencesFound array', () => {
+        let differencesHashmap = mouseService.differencesHashmap;
+        let differencesNumberFound = mouseService.differencesNumberFound;
+        mouseService.resetData;
+        expect(mouseService.differencesHashmap).to.equals(differencesHashmap);
+        expect(mouseService.differencesNumberFound).to.equals(differencesNumberFound);
+    });
+    
 });
