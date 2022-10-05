be2178e379369efa2e398aa889f2cfe73f713353
David Amoussa <henoc-jean-david.amoussa@polymtl.ca>
2022-10-04 22:27:51 -0400

    Ajout test mouse detection

--- client/src/app/services/mouse-detection.service.spec.ts
+++ client/src/app/services/mouse-detection.service.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
+import { Position } from '@common/position';
+import { DrawService } from './draw.service';
 
 import { MouseDetectionService } from './mouse-detection.service';
+import { SocketClientService } from './socket-client.service';
 
-describe('MouseDetectionService', () => {
-  let service: MouseDetectionService;
+fdescribe('MouseDetectionService', () => {
+    let service: MouseDetectionService;
+    let socketSpy: jasmine.SpyObj<SocketClientService>;
+    let drawServiceSpy: jasmine.SpyObj<DrawService>;
+    let message = 'hello world';
+    let context: CanvasRenderingContext2D;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MouseDetectionService);
-  });
+    let position: Position = { x: 0, y: 0 };
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                { provide: SocketClientService, useValue: socketSpy },
+                { provide: DrawService, useValue: drawServiceSpy },
+            ],
+        });
+        drawServiceSpy = jasmine.createSpyObj('DrawService', ['drawWord']);
+        socketSpy = jasmine.createSpyObj('SocketClientService', ['send']);
+        socketSpy.send('Verify position', position);
+        drawServiceSpy.drawWord(message, position, context);
+        service = TestBed.inject(MouseDetectionService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    // it('should call ', () => {
+    //     service.clickMessage(true);
+    //     expect(drawServiceSpy).toHaveBeenCalled();
+    // });
 });
