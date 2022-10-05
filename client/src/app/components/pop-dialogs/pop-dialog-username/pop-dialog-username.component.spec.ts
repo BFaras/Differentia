8219ab1c14cac57eaa59fa9544875e14578d05cf
David Amoussa <henoc-jean-david.amoussa@polymtl.ca>
2022-10-04 21:39:37 -0400

    Fix tests pour pour gameForm et pop-dialog

--- client/src/app/components/pop-dialogs/pop-dialog-username/pop-dialog-username.component.spec.ts
+++ client/src/app/components/pop-dialogs/pop-dialog-username/pop-dialog-username.component.spec.ts
@@ -1,23 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { SocketClientService } from '@app/services/socket-client.service';
 
 import { PopDialogUsernameComponent } from './pop-dialog-username.component';
 
-describe('PopDialogUsernameComponent', () => {
-  let component: PopDialogUsernameComponent;
-  let fixture: ComponentFixture<PopDialogUsernameComponent>;
+fdescribe('PopDialogUsernameComponent', () => {
+    let component: PopDialogUsernameComponent;
+    let fixture: ComponentFixture<PopDialogUsernameComponent>;
+    let socketSpy: jasmine.SpyObj<SocketClientService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PopDialogUsernameComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [PopDialogUsernameComponent],
+            providers: [
+                { provide: MatDialogRef, useValue: {} },
+                { provide: MAT_DIALOG_DATA, useValue: {} },
+            ],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(PopDialogUsernameComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        socketSpy = jasmine.createSpyObj('SocketClientService', ['send']);
+        fixture = TestBed.createComponent(PopDialogUsernameComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should send info to server', () => {
+        component.gamePage();
+    });
 });
