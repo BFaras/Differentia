ab1637b8a48f4bc95908b57002bb7efa3a6bed9a
BFaras <71102846+BFaras@users.noreply.github.com>
2022-10-16 04:38:52 -0400

    ajout de possiblite de pain ave bug pas dans 2 canvas

--- client/src/app/components/canvas-drawing/canvas-drawing.component.ts
+++ client/src/app/components/canvas-drawing/canvas-drawing.component.ts
@@ -1,4 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Coordinate } from '@app/interfaces/coordinate';
+import { DrawingHandlerService } from '@app/services/drawing-handler.service';
 import { IMAGE_HEIGHT, IMAGE_WIDTH } from '@common/const';
 @Component({
   selector: 'app-canvas-drawing',
@@ -10,6 +12,8 @@ export class CanvasDrawingComponent implements  AfterViewInit {
   private canvas: HTMLCanvasElement
   private cx: CanvasRenderingContext2D | null;
 
+  constructor(private drawingHandlerService:DrawingHandlerService) { }
+
   public ngAfterViewInit() {
     this.canvas = this.canvasDOM.nativeElement;
     this.cx = this.canvas.getContext('2d');
@@ -20,8 +24,29 @@ export class CanvasDrawingComponent implements  AfterViewInit {
       this.cx.lineWidth = 3;
       this.cx.lineCap = 'round';
       this.cx.strokeStyle = '#000000';
+      this.drawingHandlerService.setCanvas(this.canvas);
 
+      this.drawingHandlerService.setAllObservables();
+      this.prepareCanvasDrawing();
     }
   }
 
+  prepareCanvasDrawing() {
+    this.drawingHandlerService.startObservingMousePath()
+      .subscribe((res: any) => {
+        const canvas = this.canvas.getBoundingClientRect();
+
+        const previousCoordinate:Coordinate= {
+          x: res[0].clientX - canvas.left,
+          y: res[0].clientY - canvas.top,
+        };
+
+        const actualCoordinate:Coordinate =  {
+          x: res[1].clientX - canvas.left,
+          y: res[1].clientY - canvas.top,
+        };
+
+        this.drawingHandlerService.drawOnCanvas(previousCoordinate, actualCoordinate,this.cx!);
+      });
+  }
 }
--- client/src/app/services/drawing-handler.service.ts
+++ client/src/app/services/drawing-handler.service.ts
@@ -1,9 +1,62 @@
 import { Injectable } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { Observable } from 'rxjs';
+import { pairwise, switchMap, takeUntil } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DrawingHandlerService {
-
+  private canvas: HTMLCanvasElement
+  mouseDownObservable:Observable<any>
+  mouseMoveObservable:Observable<any>
+  mouseUpObservable:Observable<any> 
+  mouseLeaveObservable:Observable<any> 
   constructor() { }
+
+  setCanvas(canvas: HTMLCanvasElement){
+    this.canvas = canvas
+  }
+
+  setAllObservables():void{
+    this.mouseDownObservable = fromEvent( this.getCanvas(),'mousedown');
+    this.mouseMoveObservable = fromEvent( this.getCanvas(),'mousemove');
+    this.mouseUpObservable = fromEvent( this.getCanvas(),'mouseup');
+    this.mouseLeaveObservable = fromEvent( this.getCanvas(),'mouseleave');
+  }
+
+  getCanvas():HTMLCanvasElement{
+    return this.canvas;
+  }
+
+  stopObservingMousePath(){
+    return this.mouseMoveObservable.pipe(
+      takeUntil(this.mouseUpObservable),
+      takeUntil(this.mouseLeaveObservable),
+      pairwise())
+  }
+
+  startObservingMousePath() {
+    return this.mouseDownObservable
+      .pipe(
+        switchMap((e) => {
+          return  this.stopObservingMousePath();
+        })
+      )
+  }
+
+  drawOnCanvas(
+    prevPos: { x: number; y: number },
+    currentPos: { x: number; y: number },
+    cx: CanvasRenderingContext2D 
+  ) {
+    if (cx != null) {
+    cx.beginPath();
+    cx.moveTo(prevPos.x, prevPos.y);
+    cx.lineTo(currentPos.x, currentPos.y);
+    cx.stroke();
+    }
+  }
+
+
 }
