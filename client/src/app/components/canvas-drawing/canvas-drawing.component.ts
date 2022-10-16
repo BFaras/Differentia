a6bff65bc1ff77419cf1400c6369f492887a13b2
BFaras <71102846+BFaras@users.noreply.github.com>
2022-10-16 03:19:01 -0400

    ajout de creation de canvas

--- client/src/app/components/canvas-drawing/canvas-drawing.component.ts
+++ client/src/app/components/canvas-drawing/canvas-drawing.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { IMAGE_HEIGHT, IMAGE_WIDTH } from '@common/const';
 @Component({
   selector: 'app-canvas-drawing',
   templateUrl: './canvas-drawing.component.html',
-  styleUrls: ['./canvas-drawing.component.scss']
+  styleUrls: ['./canvas-drawing.component.scss'],
 })
-export class CanvasDrawingComponent  {
+export class CanvasDrawingComponent implements  AfterViewInit {
+  @ViewChild('canvas') public canvasDOM: ElementRef<HTMLCanvasElement>;
+  private canvas: HTMLCanvasElement
+  private cx: CanvasRenderingContext2D | null;
+
+  public ngAfterViewInit() {
+    this.canvas = this.canvasDOM.nativeElement;
+    this.cx = this.canvas.getContext('2d');
+
+    this.canvas.width = IMAGE_WIDTH;
+    this.canvas.height = IMAGE_HEIGHT;
+    if (this.cx != null) {
+      this.cx.lineWidth = 3;
+      this.cx.lineCap = 'round';
+      this.cx.strokeStyle = '#000000';
+
+    }
+  }
 
 }
-
-
