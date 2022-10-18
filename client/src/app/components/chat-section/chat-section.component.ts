8fe4a0b0610140dd48ea6d49f7a0ef4e854beebd
rak553 <raphael62243@gmail.com>
2022-10-18 17:27:32 -0400

    RL Début du chat dans la vue

--- client/src/app/components/chat-section/chat-section.component.ts
+++ client/src/app/components/chat-section/chat-section.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatMessage } from '@common/chat-message';
 
 @Component({
-  selector: 'app-chat-section',
-  templateUrl: './chat-section.component.html',
-  styleUrls: ['./chat-section.component.scss']
+    selector: 'app-chat-section',
+    templateUrl: './chat-section.component.html',
+    styleUrls: ['./chat-section.component.scss'],
 })
 export class ChatSectionComponent implements OnInit {
+    public messagesSent: ChatMessage[];
+    constructor() {
+        this.messagesSent = [
+            { senderName: 'Raphael', message: 'Hello you' },
+            { senderName: 'Someone101', message: 'Hello you too' },
+        ];
+    }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
---
+++ common/chat-message.ts
@@ -0,0 +1,4 @@
+export interface ChatMessage {
+    senderName: string;
+    message: string;
+}
