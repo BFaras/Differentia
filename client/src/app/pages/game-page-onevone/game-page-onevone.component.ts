31875c960becbe14d7412a8252074539c3416dff
Charles-Antoine23 <charles-antoine.vezina@polymtl.ca>
2022-10-19 00:12:15 -0400

    Ajout du code dans le component game onevone

--- client/src/app/pages/game-page-onevone/game-page-onevone.component.ts
+++ client/src/app/pages/game-page-onevone/game-page-onevone.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { SocketClientService } from '@app/services/socket-client.service';
 
 @Component({
-  selector: 'app-game-page-onevone',
-  templateUrl: './game-page-onevone.component.html',
-  styleUrls: ['./game-page-onevone.component.scss']
+    selector: 'app-game-page-onevone',
+    templateUrl: './game-page-onevone.component.html',
+    styleUrls: ['./game-page-onevone.component.scss'],
 })
 export class GamePageOnevoneComponent implements OnInit {
+    nbDifferences: number = 0;
+    gameName: string = 'defaultGameName';
+    userName: string = 'defaultUserName';
+    images: HTMLImageElement[];
+    nbDifferencesFoundPlayerA: number = 0;
+    nbDifferencesFoundPlayerB: number = 0;
 
-  constructor() { }
+    constructor(public socketService: SocketClientService /*private timeService: TimeService, private communicationService: CommunicationService*/) {
+        this.images = [new Image(640, 480), new Image(640, 480)];
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.socketService.connect();
+    }
 
+    ngOnDestroy() {
+        this.socketService.send('kill the game');
+        this.socketService.disconnect();
+    }
 }
