c5b9ef48d1e528bd17d89c168238277cf13e3c12
rak553 <raphael62243@gmail.com>
2022-10-20 14:23:31 -0400

    [Test] Test de ce qui se trouve présentement ChatMessageService

--- client/src/app/services/chat-messages.service.spec.ts
+++ client/src/app/services/chat-messages.service.spec.ts
@@ -1,22 +1,43 @@
 import { TestBed } from '@angular/core/testing';
 import { SocketTestHelper } from '@app/classes/socket-test-helper';
+import { ChatMessage } from '@common/chat-message';
+import { Subscription } from 'rxjs';
 
 import { Socket } from 'socket.io-client';
 import { ChatMessagesService } from './chat-messages.service';
 import { SocketClientService } from './socket-client.service';
 
 describe('ChatMessagesService', () => {
-  let chatMessageService: ChatMessagesService;
+  const emptySubcriberCallbackTest = (message : ChatMessage) => {};
+  const defaultMessageTest : ChatMessage = {senderName : "Player1", message : "Hello"};
+  let chatMessagesService: ChatMessagesService;
   let socketService : SocketClientService;
+  let observer : Subscription;
+  let socketTestHelper : SocketTestHelper;
 
-  beforeEach(() => {
+  beforeAll(() => {
     TestBed.configureTestingModule({});
-    chatMessageService = TestBed.inject(ChatMessagesService);
     socketService = TestBed.inject(SocketClientService);
-    socketService.socket = (new SocketTestHelper() as unknown) as Socket;
+    chatMessagesService = TestBed.inject(ChatMessagesService);
+    socketTestHelper = new SocketTestHelper();
+    socketService.socket = (socketTestHelper as unknown) as Socket;
   });
 
-  it('should be created', () => {
-    expect(chatMessageService).toBeTruthy();
+  it('should create an observable which we can subscribe to on construction', () => {
+    observer = chatMessagesService.messagesObservable.subscribe(emptySubcriberCallbackTest);
+    expect(observer).toBeTruthy;
+  });
+
+  it('should call create an observable and when we subscribe the callback will be called', () => {
+    observer = chatMessagesService.messagesObservable.subscribe(emptySubcriberCallbackTest);
+    expect(emptySubcriberCallbackTest).toHaveBeenCalled;
+  });
+
+  it('should call the callback when someone is subscribed to the observable and a Valid click event is sent', async () => {
+    observer = chatMessagesService.messagesObservable.subscribe(emptySubcriberCallbackTest);
+    socketTestHelper.peerSideEmit('Valid click', defaultMessageTest);
+    await setTimeout(()=>{
+      expect(emptySubcriberCallbackTest).toHaveBeenCalled;
+    })
   });
 });
