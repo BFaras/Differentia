e57c4c739ba0726ba721f2e08ad23fca21e56e4d
rak553 <raphael62243@gmail.com>
2022-10-22 13:46:08 -0400

    [Feature] Ajout d'une fonction pour générer les messages système et ajout des messages de différence en multijoueur

--- client/src/app/services/chat-messages.service.ts
+++ client/src/app/services/chat-messages.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ChatMessage } from '@common/chat-message';
-import { GAME_MESSAGE_SENDER_NAME, MESSAGE_DIFFERENCE_FOUND_DEFAULT, MESSAGE_ERROR_DIFFERENCE_DEFAULT, TWO_DIGIT_TIME_VALUE } from '@common/const';
+import {
+    GAME_MESSAGE_SENDER_NAME,
+    MESSAGE_DIFFERENCE_FOUND_MULTI,
+    MESSAGE_DIFFERENCE_FOUND_SOLO,
+    MESSAGE_ERROR_DIFFERENCE_MULTI,
+    MESSAGE_ERROR_DIFFERENCE_SOLO,
+    TWO_DIGIT_TIME_VALUE,
+} from '@common/const';
 import { GameplayDifferenceInformations } from '@common/gameplay-difference-informations';
 import { Observable, Subscriber } from 'rxjs';
 import { SocketClientService } from './socket-client.service';
@@ -31,12 +38,24 @@ export class ChatMessagesService {
 
     private configureSocket(observer: Subscriber<ChatMessage>) {
         this.socketService.on('Valid click', (differenceInfos: GameplayDifferenceInformations) => {
-            if (differenceInfos.isValidDifference) {
-                observer.next(this.generateChatMessage(this.getTimeInCorrectFormat(), GAME_MESSAGE_SENDER_NAME, MESSAGE_DIFFERENCE_FOUND_DEFAULT));
-            } else {
-                observer.next(this.generateChatMessage(this.getTimeInCorrectFormat(), GAME_MESSAGE_SENDER_NAME, MESSAGE_ERROR_DIFFERENCE_DEFAULT));
+            if (differenceInfos.isValidDifference && this.isMultiplayerGame) {
+                observer.next(
+                    this.generateChatMessageFromGame(this.getTimeInCorrectFormat(), MESSAGE_DIFFERENCE_FOUND_MULTI + differenceInfos.playerName),
+                );
+            } else if (differenceInfos.isValidDifference && !this.isMultiplayerGame) {
+                observer.next(this.generateChatMessageFromGame(this.getTimeInCorrectFormat(), MESSAGE_DIFFERENCE_FOUND_SOLO));
+            } else if (!differenceInfos.isValidDifference && this.isMultiplayerGame) {
+                observer.next(
+                    this.generateChatMessageFromGame(this.getTimeInCorrectFormat(), MESSAGE_ERROR_DIFFERENCE_MULTI + differenceInfos.playerName),
+                );
+            } else if (!differenceInfos.isValidDifference && this.isMultiplayerGame) {
+                observer.next(this.generateChatMessageFromGame(this.getTimeInCorrectFormat(), MESSAGE_ERROR_DIFFERENCE_SOLO));
             }
         });
+    }
+
+    private generateChatMessageFromGame(timeMessageSent: string, message: string) {
+        return this.generateChatMessage(timeMessageSent, GAME_MESSAGE_SENDER_NAME, message);
     }
 
     private generateChatMessage(timeMessageSent: string, senderName: string, message: string): ChatMessage {
