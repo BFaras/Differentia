7bbcf1e492a7ca95b40c8a15f565939aff592f04
David Amoussa <henoc-jean-david.amoussa@polymtl.ca>
2022-10-22 17:45:46 -0400

    Test du PlayArea: coverage --> 52%,0%,75%,51%

--- client/src/app/components/play-area/play-area.component.spec.ts
+++ client/src/app/components/play-area/play-area.component.spec.ts
@@ -1,11 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialog } from '@angular/material/dialog';
+import { SocketTestHelper } from '@app/classes/socket-test-helper';
 import { ImageGeneratorService } from '@app/services/difference-detector-feature/image-generator.service';
 import { DrawService } from '@app/services/draw.service';
 import { ImageToImageDifferenceService } from '@app/services/image-to-image-difference.service';
 import { MouseDetectionService } from '@app/services/mouse-detection.service';
 import { SocketClientService } from '@app/services/socket-client.service';
 import { Position } from '@common/position';
+import { Socket } from 'socket.io-client';
 import { PlayAreaComponent } from './play-area.component';
 import SpyObj = jasmine.SpyObj;
 describe('PlayAreaComponent', () => {
@@ -19,6 +21,8 @@ describe('PlayAreaComponent', () => {
     let imageDifferenceSpy: SpyObj<ImageToImageDifferenceService>;
     let mouseEvent: MouseEvent;
     let position: Position = { x: 10, y: 20 };
+    let differenceImage: HTMLImageElement[] = [new Image(640, 480)];
+    let socketTestHelper: SocketTestHelper;
 
     beforeAll(async () => {
         socketServiceSpy = jasmine.createSpyObj('SocketClientService', ['connect', 'on', 'send']);
@@ -43,27 +47,29 @@ describe('PlayAreaComponent', () => {
         }).compileComponents();
     });
 
-    beforeEach(() => {
+    beforeEach(async () => {
         fixture = TestBed.createComponent(PlayAreaComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
+        component.differentImages = differenceImage;
+        const socketService = TestBed.inject(SocketClientService);
+        socketTestHelper = new SocketTestHelper();
+
+        socketService.socket = socketTestHelper as unknown as Socket;
+
+        //Dans le tests
+        //socketTestHelper.peerSideEmit('Valid click');
     });
 
     it('should display images ', () => {
         component.displayImages();
         fixture.detectChanges();
-        expect(drawServiceSpy).toBeDefined();
+        expect(drawServiceSpy).toHaveBeenCalled();
     });
 
     it('should open dialog', () => {
         component.openDialog();
         expect(matDialogSpy).toBeTruthy();
-    });
-
-    it('should get height', () => {
-        const canvasSpy = spyOn(component.clickCanvas1.nativeElement, 'getContext').and.callThrough();
-        component.displayImages();
-        expect(canvasSpy).toBeTruthy();
     });
 
     it('should detect mouseEvent  ', () => {
