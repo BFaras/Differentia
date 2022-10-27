bedab56e1a65e71a24fcb26991a7257fac89e959
Charles-Antoine23 <charles-antoine.vezina@polymtl.ca>
2022-10-27 15:43:10 -0400

    [fix] deploiement

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-35-183-207-177.ca-central-1.compute.amazonaws.com:3000', // TODO: Add remote server URL
+    serverUrl: 'http://ec2-15-222-3-41.ca-central-1.compute.amazonaws.com:3000', // TODO: Add remote server URL
 };
