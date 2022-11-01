758c930f9de0727fe1daf24c97a79f7df29773de
rak553 <raphael62243@gmail.com>
2022-11-01 16:17:22 -0400

    [Fix] Correction acceurs GamePageComp mis socketService private

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -21,7 +21,7 @@ export class GamePageComponent {
     images: HTMLImageElement[];
     nbDifferrencesFound: number[] = [0, 0];
 
-    constructor(public socketService: SocketClientService, private timeService: TimeService, private communicationService: CommunicationService) {
+    constructor(private socketService: SocketClientService, private timeService: TimeService, private communicationService: CommunicationService) {
         this.images = [new Image(640, 480), new Image(640, 480)];
     }
 
