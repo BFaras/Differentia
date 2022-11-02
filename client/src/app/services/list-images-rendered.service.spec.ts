d69ca89141f7a3f02a9cae6581b8623833de171b
rak553 <raphael62243@gmail.com>
2022-11-02 17:11:23 -0400

    [Fix] Correction des accesseurs de ListImagesRenderedService

--- client/src/app/services/list-images-rendered.service.spec.ts
+++ client/src/app/services/list-images-rendered.service.spec.ts
@@ -3,31 +3,27 @@ import { TestBed } from '@angular/core/testing';
 import { ListImagesRenderedService } from './list-images-rendered.service';
 
 describe('ListImagesRenderedService', () => {
-  let service: ListImagesRenderedService;
+    let service: ListImagesRenderedService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ListImagesRenderedService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(ListImagesRenderedService);
+    });
 
-  it('should emit a number through activatedEmitterRemoveImage ', () => {
-    let fakeNumber:number = 10;
-    expect(service.sendIdImageToRemove(fakeNumber)).toEqual(service.activatedEmitterRemoveImage.emit(fakeNumber))
-  });
+    it('should emit a number through activatedEmitterRemoveImage ', () => {
+        let fakeNumber: number = 10;
+        expect(service.sendIdImageToRemove(fakeNumber)).toEqual(service['activatedEmitterRemoveImage'].emit(fakeNumber));
+    });
 
-  it('should return activitedEmitter when calling getIdImageToRemove ', () => {
+    it('should return activitedEmitter when calling getIdImageToRemove ', () => {
+        expect(service.getIdImageToRemove()).toEqual(service['activatedEmitterRemoveImage']);
+    });
 
-    expect(service.getIdImageToRemove()).toEqual(service.activatedEmitterRemoveImage)
-  });
+    it('should return activatedEmitterUrlImageSingle when calling getIdImageToRemove ', () => {
+        expect(service.getDataImageSingle()).toEqual(service['activatedEmitterUrlImageSingle']);
+    });
 
-  it('should return activatedEmitterUrlImageSingle when calling getIdImageToRemove ', () => {
-
-    expect(service.getDataImageSingle()).toEqual(service.activatedEmitterUrlImageSingle)
-  });
-
-  it('should return activitedEmitter when calling sendIdImageToRemove ', () => {
-
-    expect(service.getDataImageMultiple()).toEqual(service.activatedEmitterUrlImageBoth)
-
-  });
+    it('should return activitedEmitter when calling sendIdImageToRemove ', () => {
+        expect(service.getDataImageMultiple()).toEqual(service['activatedEmitterUrlImageBoth']);
+    });
 });
