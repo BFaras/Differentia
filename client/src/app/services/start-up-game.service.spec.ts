aaa7cfb0c1d45b27dc5020ebfd3c4e4088ab1a8e
IsmaelAs <50330186+IsmaelAs@users.noreply.github.com>
2022-11-05 02:49:02 -0400

    Test- StartUpGameService fini

--- client/src/app/services/start-up-game.service.spec.ts
+++ client/src/app/services/start-up-game.service.spec.ts
@@ -1,36 +1,69 @@
 import { TestBed } from '@angular/core/testing';
-//import { Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { SocketTestHelper } from '@app/classes/socket-test-helper';
+import { DEFAULT_USERNAME } from '@common/const';
 import { Socket } from 'socket.io-client';
-//import { CreateGameService } from './create-game.service';
-//import { JoinGameService } from './join-game.service';
-import { RouterTestingModule } from '@angular/router/testing';
+import { CreateGameService } from './create-game.service';
+import { JoinGameService } from './join-game.service';
 import { SocketClientService } from './socket-client.service';
 import { StartUpGameService } from './start-up-game.service';
 
-fdescribe('StartUpGameService', () => {
+describe('StartUpGameService', () => {
     const testGameName = 'Test Game';
+    const multiPlayerGameInfo: any = {
+        isPlayerWaiting: true,
+        multiFlag: true,
+    };
+    const createGameInfo: any = {
+        isPlayerWaiting: false,
+        multiFlag: false,
+    };
+    let routerSpy = { navigate: jasmine.createSpy('navigate') };
     let startUpGameService: StartUpGameService;
-    //  let createGameService: CreateGameService;
-    //let joinGameService: JoinGameService;
+    let createGameService: CreateGameService;
+    let joinGameService: JoinGameService;
     let socketService: SocketClientService;
-    //let router: Router;
     let socketTestHelper: SocketTestHelper;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [RouterTestingModule],
+            providers: [{ provide: Router, useValue: routerSpy }],
         });
+
         startUpGameService = TestBed.inject(StartUpGameService);
-        // createGameService = TestBed.inject(CreateGameService);
+        createGameService = TestBed.inject(CreateGameService);
         socketService = TestBed.inject(SocketClientService);
-        //joinGameService = TestBed.inject(JoinGameService);
+        joinGameService = TestBed.inject(JoinGameService);
         socketTestHelper = new SocketTestHelper();
         socketService.socket = socketTestHelper as unknown as Socket;
     });
 
     it('should be created', () => {
         expect(startUpGameService).toBeTruthy();
+    });
+
+    it('should multiplayerGame() call joinGame() of JoinGameService if a player is waiting ', () => {
+        const spy = spyOn(joinGameService, 'joinGame').and.callThrough();
+        startUpGameService['multiplayerGame'](multiPlayerGameInfo, DEFAULT_USERNAME);
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('should multiplayerGame() call createGame() of CreateGameService if no player is waiting ', () => {
+        const spy = spyOn(createGameService, 'createGame').and.callThrough();
+        startUpGameService['multiplayerGame'](createGameInfo, DEFAULT_USERNAME);
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('should startUpWaitingLine() call multiplayerGame() of StartUpGameService if there is the multiplayer flag', () => {
+        const spy = spyOn(startUpGameService, <any>'multiplayerGame').and.callThrough();
+        startUpGameService.startUpWaitingLine(multiPlayerGameInfo, DEFAULT_USERNAME);
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('should startUpWaitingLine() call soloGame() of StartUpGameService if there is not the multiplayer flag', () => {
+        const spy = spyOn(startUpGameService, <any>'soloGame').and.callThrough();
+        startUpGameService.startUpWaitingLine(createGameInfo, DEFAULT_USERNAME);
+        expect(spy).toHaveBeenCalled();
     });
 
     it('should soloGame call send()', () => {
@@ -51,9 +84,14 @@ fdescribe('StartUpGameService', () => {
         expect(spy).toHaveBeenCalled();
     });
 
-    /*it('should startMatch() call send', () => {
-      const spy = spyOn(socketService, 'send').and.callThrough();
-      startUpGameService.startMatch(testGameName);
-      expect(spy).toHaveBeenCalled();
-  });*/
+    it('should startMatch() call send()', () => {
+        const spy = spyOn(socketService, 'send').and.callThrough();
+        startUpGameService.startMatch(testGameName);
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('should startMatch() navigate to the right URL', () => {
+        startUpGameService.startMatch(testGameName);
+        expect(routerSpy.navigate).toHaveBeenCalledWith(['/game']);
+    });
 });
