110bd3d298ef5e825a7d236ee766a4717321a9bb
Charles-Antoine23 <charles-antoine.vezina@polymtl.ca>
2022-11-06 13:44:02 -0500

    [tes] Test pour pencil service terminer

--- client/src/app/services/pencil.service.spec.ts
+++ client/src/app/services/pencil.service.spec.ts
@@ -1,16 +1,127 @@
 import { TestBed } from '@angular/core/testing';
+import { CanvasTestHelper } from '@app/classes/canvas-test-helper';
+import { IMAGE_HEIGHT, IMAGE_WIDTH } from '@common/const';
 
 import { PencilService } from './pencil.service';
 
-describe('PencilService', () => {
-  let service: PencilService;
+fdescribe('PencilService', () => {
+    let service: PencilService;
+    let mainCanvas: HTMLCanvasElement;
+    let contextMock: CanvasRenderingContext2D;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PencilService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(PencilService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    beforeAll(() => {
+        mainCanvas = CanvasTestHelper.createCanvas(IMAGE_HEIGHT, IMAGE_WIDTH);
+        contextMock = mainCanvas.getContext('2d')!;
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should obtain the pencil color and width of the left canvas', () => {
+        const left = 0;
+        const leftColor = 'blue';
+        const leftWidth = 3;
+
+        service['leftCanvasColor'] = leftColor;
+        service['leftCanvasWidth'] = leftWidth;
+
+        expect(service.obtainPencilColor(left)).toEqual(leftColor);
+        expect(service.obtainPencilWidth(left)).toEqual(leftWidth);
+    });
+
+    it('should obtain  the pencil color and width of the right canvas', () => {
+        const right = 1;
+        const rightColor = 'blue';
+        const rightWidth = 3;
+
+        service['rightCanvasColor'] = rightColor;
+        service['rightCanvasWidth'] = rightWidth;
+
+        expect(service.obtainPencilColor(right)).toEqual(rightColor);
+        expect(service.obtainPencilWidth(right)).toEqual(rightWidth);
+    });
+
+    it('should set the pencil color and width of the left canvas', () => {
+        const left = 0;
+        const leftColor = 'red';
+        const leftWidth = 3;
+
+        service.setColor(leftColor, left);
+        service.setWidth(leftWidth, left);
+
+        expect(service['leftCanvasColor']).toEqual(leftColor);
+        expect(service['leftCanvasWidth']).toEqual(leftWidth);
+    });
+
+    it('should set the pencil color and width of the right canvas', () => {
+        const right = 1;
+        const rightColor = 'red';
+        const rightWidth = 3;
+
+        service.setColor(rightColor, right);
+        service.setWidth(rightWidth, right);
+
+        expect(service['rightCanvasColor']).toEqual(rightColor);
+        expect(service['rightCanvasWidth']).toEqual(rightWidth);
+    });
+
+    it('should assign the left canvas with the right line cap', () => {
+        const leftRes = 'round';
+        const left = 0;
+
+        service['pencilMode'] = ['write', 'erase'];
+
+        expect(service.assignRightLineCap(left)).toEqual(leftRes);
+    });
+
+    it('should assign the right canvas with the right line cap', () => {
+        const rightRes = 'square';
+        const right = 1;
+
+        service['pencilMode'] = ['write', 'erase'];
+
+        expect(service.assignRightLineCap(right)).toEqual(rightRes);
+    });
+
+    it('should not assign a canvas with the wrong line cap', () => {
+        const index = 0;
+
+        service['pencilMode'] = ['wrongLineCap'];
+
+        expect(service.assignRightLineCap(index)).toBeUndefined();
+    });
+
+    it('should the state of the pencil for the right canvas', () => {
+        const pencilMode = 'write';
+        const left = 0;
+
+        service['pencilMode'] = ['erase', 'erase'];
+        service.setStateOfPencilForRightCanvas(pencilMode, left);
+
+        expect(service['pencilMode'][left]).toEqual(pencilMode);
+    });
+
+    it('sould get the state of the pencil on the left canvas', () => {
+        const left = 0;
+
+        service['pencilMode'] = ['write', 'erase'];
+        service.getStateOfPencil(contextMock, left);
+
+        expect(contextMock.globalCompositeOperation).toEqual('source-over');
+    });
+
+    it('sould get the state of the pencil on the left canvas', () => {
+        const right = 1;
+
+        service['pencilMode'] = ['write', 'erase'];
+        service.getStateOfPencil(contextMock, right);
+
+        expect(contextMock.globalCompositeOperation).toEqual('destination-out');
+    });
 });
--- client/src/app/services/pencil.service.ts
+++ client/src/app/services/pencil.service.ts
@@ -1,83 +1,72 @@
 import { Injectable } from '@angular/core';
 
-const ROUND_LINE_CAP:CanvasLineCap = "round";
-const SQUARE_LINE_CAP:CanvasLineCap = "square";
-const WRITE_MODE:string = "write";
-const ERASE_MODE:string = "erase";
+const ROUND_LINE_CAP: CanvasLineCap = 'round';
+const SQUARE_LINE_CAP: CanvasLineCap = 'square';
+const WRITE_MODE: string = 'write';
+const ERASE_MODE: string = 'erase';
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class PencilService {
-  private leftCanvasWidth:number;
-  private leftCanvasColor:string;
-  private RightCanvasWidth:number;
-  private RightCanvasColor:string;
-  private pencilMode:string[];
+    private leftCanvasWidth: number;
+    private leftCanvasColor: string;
+    private rightCanvasWidth: number;
+    private rightCanvasColor: string;
+    private pencilMode: string[];
 
-  constructor() {
-    this.pencilMode = [];
-   }
-
-  obtainPencilColor(index:number):string{
-    if(index == 0){
-      return this.leftCanvasColor
-    }
-    else 
-    {
-      return this.RightCanvasColor
-    }
-  }
-
-  setColor(color:string,index:number):void{
-    if (index == 0)
-      {
-        this.leftCanvasColor = color
-      }
-    else if(index == 1)
-      {this.RightCanvasColor = color};
-
-  }
-
-  setWidth(width:number,index:number):void{
-    
-    if (index == 0)
-      {this.leftCanvasWidth = width}
-    else if(index == 1)
-      {this.RightCanvasWidth = width};
-  }
-
-  obtainPencilWidth(index:number):number{
-    if(index == 0){
-      return this.leftCanvasWidth
-    }
-    else 
-    {
-      return this.RightCanvasWidth
-    }
-  }
-
-  assignRightLineCap(indexCanvas:number){
-    if(this.pencilMode[indexCanvas] == WRITE_MODE){
-      return ROUND_LINE_CAP
+    constructor() {
+        this.pencilMode = [];
     }
 
-    else if(this.pencilMode[indexCanvas] == ERASE_MODE){
-      return SQUARE_LINE_CAP
+    obtainPencilColor(index: number): string {
+        if (index == 0) {
+            return this.leftCanvasColor;
+        } else {
+            return this.rightCanvasColor;
+        }
     }
-    return
-    
-  }
 
-  setStateOfPencilForRightCanvas(pencilMode:string,indexCanvas:number){
-    this.pencilMode[indexCanvas] = pencilMode;
-  }
-
-  getStateOfPencil(context:CanvasRenderingContext2D,indexCanvas:number){
-    if(this.pencilMode[indexCanvas] == ERASE_MODE)
-      context.globalCompositeOperation = "destination-out";
-    if (this.pencilMode[indexCanvas] == WRITE_MODE){
-      context.globalCompositeOperation = "source-over";
+    setColor(color: string, index: number): void {
+        if (index == 0) {
+            this.leftCanvasColor = color;
+        } else if (index == 1) {
+            this.rightCanvasColor = color;
+        }
     }
-  }
 
+    setWidth(width: number, index: number): void {
+        if (index == 0) {
+            this.leftCanvasWidth = width;
+        } else if (index == 1) {
+            this.rightCanvasWidth = width;
+        }
+    }
+
+    obtainPencilWidth(index: number): number {
+        if (index == 0) {
+            return this.leftCanvasWidth;
+        } else {
+            return this.rightCanvasWidth;
+        }
+    }
+
+    assignRightLineCap(indexCanvas: number) {
+        if (this.pencilMode[indexCanvas] == WRITE_MODE) {
+            return ROUND_LINE_CAP;
+        } else if (this.pencilMode[indexCanvas] == ERASE_MODE) {
+            return SQUARE_LINE_CAP;
+        }
+        return;
+    }
+
+    setStateOfPencilForRightCanvas(pencilMode: string, indexCanvas: number) {
+        this.pencilMode[indexCanvas] = pencilMode;
+    }
+
+    getStateOfPencil(context: CanvasRenderingContext2D, indexCanvas: number) {
+        if (this.pencilMode[indexCanvas] == ERASE_MODE) context.globalCompositeOperation = 'destination-out';
+        if (this.pencilMode[indexCanvas] == WRITE_MODE) {
+            context.globalCompositeOperation = 'source-over';
+        }
+    }
 }
