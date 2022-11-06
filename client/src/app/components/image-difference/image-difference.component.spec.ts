1fba7dbcfa53b7c0822156a60e1e91b50d2588cf
rak553 <raphael62243@gmail.com>
2022-11-06 11:05:29 -0500

    [Test] Ajout test pour l'évènement game creation differences informations

--- client/src/app/components/image-difference/image-difference.component.spec.ts
+++ client/src/app/components/image-difference/image-difference.component.spec.ts
@@ -4,10 +4,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SafeValue } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Renderer2TestHelper } from '@app/classes/renderer2-test-helper';
+import { SocketTestHelper } from '@app/classes/socket-test-helper';
 import { GameToServerService } from '@app/services/game-to-server.service';
 import { ImageToImageDifferenceService } from '@app/services/image-to-image-difference.service';
 import { SocketClientService } from '@app/services/socket-client.service';
+import { DifferencesInformations } from '@common/differences-informations';
 import { ImageToSendToServer } from '@common/imageToSendToServer';
+import { Socket } from 'socket.io-client';
 import { ImageDifferenceComponent } from './image-difference.component';
 
 fdescribe('ImageDifferenceComponent', () => {
@@ -19,11 +22,15 @@ fdescribe('ImageDifferenceComponent', () => {
 
     let component: ImageDifferenceComponent;
     let fixture: ComponentFixture<ImageDifferenceComponent>;
-    let getImagesDataSpy: jasmine.Spy;
     let gameToServerService: GameToServerService;
+    let socketService: SocketClientService;
+    let socketTestHelper: SocketTestHelper;
+    let imageToImageDifferenceService: ImageToImageDifferenceService;
     let renderer: Renderer2;
+    let getImagesDataSpy: jasmine.Spy;
 
     beforeEach(async () => {
+        socketTestHelper = new SocketTestHelper();
         renderer = new Renderer2TestHelper() as unknown as Renderer2;
 
         await TestBed.configureTestingModule({
@@ -34,12 +41,16 @@ fdescribe('ImageDifferenceComponent', () => {
 
         fixture = TestBed.createComponent(ImageDifferenceComponent);
         gameToServerService = fixture.debugElement.injector.get(GameToServerService);
+        imageToImageDifferenceService = fixture.debugElement.injector.get(ImageToImageDifferenceService);
+        socketService = fixture.debugElement.injector.get(SocketClientService);
+        socketService.socket = socketTestHelper as unknown as Socket;
+
         component = fixture.componentInstance;
 
         spyOn(gameToServerService, 'getOriginalImageUploaded').and.returnValue(mockImage);
         spyOn(gameToServerService, 'getModifiedImageUploaded').and.returnValue(mockImage);
-        spyOn(ImageToImageDifferenceService.prototype, 'waitForImageToLoad').and.returnValue(Promise.resolve());
-        getImagesDataSpy = spyOn(ImageToImageDifferenceService.prototype, 'getImagesData').and.returnValue({
+        spyOn(imageToImageDifferenceService, 'waitForImageToLoad').and.returnValue(Promise.resolve());
+        getImagesDataSpy = spyOn(imageToImageDifferenceService, 'getImagesData').and.returnValue({
             originalImageData: new Uint8ClampedArray(),
             modifiedImageData: new Uint8ClampedArray(),
             imageHeight: 0,
@@ -64,7 +75,7 @@ fdescribe('ImageDifferenceComponent', () => {
         expect(component.loaded()).toBeFalsy();
     });
 
-    it('should call functions from GameToServerService when there is an image source and a number of differences and loaded() is called', async () => {
+    it('should call functions from GameToServerService when there is an image source and a number of differences and loaded() is called', () => {
         const spy1 = spyOn(gameToServerService, 'setNumberDifference');
         const spy2 = spyOn(gameToServerService, 'setUrlImageOfDifference');
         const spy3 = spyOn(gameToServerService, 'setDifferencesList');
@@ -79,14 +90,31 @@ fdescribe('ImageDifferenceComponent', () => {
     });
 
     it('should call getImagesData from ImageToImageDifferenceService on ngOnInit', async () => {
-        //const spy = spyOn(ImageToImageDifferenceService.prototype, 'getImagesData');
         await component.ngOnInit();
         expect(getImagesDataSpy).toHaveBeenCalled();
     });
 
     it('should send an event to the server on ngOnInit', async () => {
-        const spy = spyOn(SocketClientService.prototype, 'send').and.callFake(() => {});
+        const spy = spyOn(socketService, 'send').and.callFake(() => {});
         await component.ngOnInit();
         expect(spy).toHaveBeenCalled();
+    });
+
+    it('should call putDifferencesDataInImage() from ImageToImageDifferenceService on a game creation differences informations event', async () => {
+        const spy = spyOn(imageToImageDifferenceService, 'putDifferencesDataInImage').and.callFake(() => {});
+        const differencesListTest = [
+            [0, 1, 5, 8, 9],
+            [100, 101, 102, 105],
+        ];
+        const differencesInformationsTest: DifferencesInformations = {
+            differencesList: differencesListTest,
+            nbOfDifferences: nbDifferencesTest,
+        };
+
+        await component.ngOnInit();
+        socketTestHelper.peerSideEmit('game creation differences informations', differencesInformationsTest);
+        await setTimeout(() => {
+            expect(spy).toHaveBeenCalled();
+        }, 100);
     });
 });
