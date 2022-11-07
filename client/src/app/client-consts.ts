aa1fe76719e4fa6615b699be73079616c58e8320
IsmaelAs <50330186+IsmaelAs@users.noreply.github.com>
2022-11-07 04:10:03 -0500

    Ajout test socketManager

--- client/src/app/client-consts.ts
+++ client/src/app/client-consts.ts
@@ -1,5 +1,4 @@
 // Constantes du game Form components
-
 export const ADMIN_GAME_FORMS_BUTTON = ['Supprimer', 'Réinitialiser'];
 export const SELECTION_GAME_FORMS_BUTTON = ['Créer', 'Jouer', 'Joindre'];
 export const MULTIPLAYER_MODE = true;
--- server/app/services/socketManager.service.spec.ts
+++ server/app/services/socketManager.service.spec.ts
@@ -11,6 +11,7 @@ import { DifferenceDetectorService } from './difference-detector.service';
 import { GameManagerService } from './game-manager.service';
 import { MouseHandlerService } from './mouse-handler.service';
 import { SocketManager } from './socketManager.service';
+import { WaitingLineHandlerService } from './waiting-line-handler.service';
 
 // À switch dans le fichier des constantes
 const RESPONSE_DELAY = 200;
@@ -31,7 +32,7 @@ describe('SocketManager service tests', () => {
     let mouseHandlerService: MouseHandlerService = new MouseHandlerService();
     let gameManagerServiceBeginGameStub: sinon.SinonStub<[socket: io.Socket, gameName: string, adversarySocket?: io.Socket], Promise<void>>;
     let gameManagerServiceClickResponseStub: sinon.SinonStub<[socket: io.Socket, mousePos: Position], void>;
-
+    let waitingLineHandlerService: WaitingLineHandlerService = new WaitingLineHandlerService();
     const urlString = 'http://localhost:3000';
 
     before(async () => {
@@ -79,6 +80,39 @@ describe('SocketManager service tests', () => {
         });
     });
 
+    it("should handle 'my username is' when the username is valid", (done) => {
+        const validUsername = "user1234";
+        const stubUsernamePlayer = sinon.stub(WaitingLineHandlerService.prototype, <any>'setUsernamePlayer').callsFake(() => {});
+        clientSocket.emit('my username is', validUsername);
+        setTimeout(() => {
+            expect(stubUsernamePlayer.calledOnce);
+            done();
+        }, RESPONSE_DELAY * 5);
+    });
+
+    it("should handle 'my username is' when the username is invalid", (done) => {
+        const invalidUsername = " user1234";
+        const emitSpy = sinon.spy(clientSocket, 'emit');
+        clientSocket.emit('my username is', invalidUsername);
+        setTimeout(() => {
+            expect(emitSpy.alwaysCalledWithExactly("username not valid"));
+            done();
+        }, RESPONSE_DELAY * 5);
+    });
+
+    it("should handle 'I am waiting event' and call addCreatingPlayer", (done) => {
+        const addCreatingPlayerSpy = sinon.spy(waitingLineHandlerService, 'addCreatingPlayer');
+        clientSocket.emit('I am waiting', testGameName);
+        const emitSpy = sinon.spy(clientSocket, 'emit');
+        setTimeout(() => {
+            expect(addCreatingPlayerSpy.calledOnce).to.be.true;
+            console.log(addCreatingPlayerSpy.called);
+            expect(emitSpy.alwaysCalledWithExactly(`${testGameName} let me tell you if someone is waiting`, true)).to.be.true;
+            console.log(emitSpy.alwaysCalledWithExactly(`${testGameName} let me tell you if someone is waiting`, true));
+            done();
+        }, RESPONSE_DELAY * 5);
+    });
+
     // Test passe pas
     it("should handle 'username is' event and emit a show the username event", (done) => {
         const usernameTest = 'Test username';
@@ -116,6 +150,11 @@ describe('SocketManager service tests', () => {
             expect(gameManagerServiceBeginGameStub.calledOnce);
             done();
         }, RESPONSE_DELAY * 5); // 1 seconde
+    });
+
+    it("should handle 'is there someone waiting' event", (done) => {
+        clientSocket.emit('is there someone waiting', testGameName);
+        done();
     });
 
     it("should handle 'detect images difference' event and call generateDifferencesList", (done) => {
