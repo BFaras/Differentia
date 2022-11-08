130ba3b040ed77bbcb30088c721c1005e039c6d3
BFaras <71102846+BFaras@users.noreply.github.com>
2022-11-08 18:55:17 -0500

    [test] ajout test list Image rendered

--- client/src/app/services/list-images-rendered.service.spec.ts
+++ client/src/app/services/list-images-rendered.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { ImageRenderedInformations } from '@app/interfaces/image-rendered-informations';
 
 import { ListImagesRenderedService } from './list-images-rendered.service';
 
@@ -12,7 +13,14 @@ describe('ListImagesRenderedService', () => {
 
     it('should emit a number through activatedEmitterRemoveImage ', () => {
         let fakeNumber: number = 10;
+        let fakeUrl: string = "fake";
+        let fakeInfo: ImageRenderedInformations = {
+            url:fakeUrl,
+            index:fakeNumber
+        }
         expect(service.sendIdImageToRemove(fakeNumber)).toEqual(service['activatedEmitterRemoveImage'].emit(fakeNumber));
+        expect(service.sendUrlImageBoth(fakeUrl)).toEqual(service['activatedEmitterUrlImageBoth'].emit(fakeUrl));
+        expect(service.sendUrlImageSingle(fakeInfo)).toEqual(service['activatedEmitterUrlImageSingle'].emit(fakeInfo));
     });
 
     it('should return activitedEmitter when calling getIdImageToRemove ', () => {
