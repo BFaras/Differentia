ab7e5c0659a1f8a35f12b9af9fc781345eb228ab
rak553 <raphael62243@gmail.com>
2022-11-08 18:10:27 -0500

    [Fix] Tests de PopDialogValidateGameComp

--- client/src/app/components/pop-dialogs/pop-dialog-validate-game/pop-dialog-validate-game.component.spec.ts
+++ client/src/app/components/pop-dialogs/pop-dialog-validate-game/pop-dialog-validate-game.component.spec.ts
@@ -1,58 +1,54 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MergeImageCanvasHandlerService } from '@app/services/merge-image-canvas-handler.service';
 import { PopDialogValidateGameComponent } from './pop-dialog-validate-game.component';
 
 describe('PopDialogValidateGameComponent', () => {
-  let component: PopDialogValidateGameComponent;
-  let fixture: ComponentFixture<PopDialogValidateGameComponent>;
-  let dialogRefSpyObj = jasmine.createSpyObj({ close: null });
-  let dialogSpy: jasmine.Spy;
-  dialogRefSpyObj.componentInstance = { body: '' };
+    let component: PopDialogValidateGameComponent;
+    let fixture: ComponentFixture<PopDialogValidateGameComponent>;
+    let dialogRefSpyObj = jasmine.createSpyObj({ close: null });
+    let dialogSpy: jasmine.Spy;
+    dialogRefSpyObj.componentInstance = { body: '' };
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PopDialogValidateGameComponent ],
-      imports:[MatDialogModule]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        const mergeImageCanvasServiceMock = jasmine.createSpyObj('MergeImageCanvasHandlerService', ['resetCanvas']);
 
-    fixture = TestBed.createComponent(PopDialogValidateGameComponent);
-    dialogSpy = spyOn(TestBed.inject(MatDialog), 'open')
-    .and.returnValue(dialogRefSpyObj);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        await TestBed.configureTestingModule({
+            declarations: [PopDialogValidateGameComponent],
+            imports: [MatDialogModule],
+            providers: [{ provide: MergeImageCanvasHandlerService, useValue: mergeImageCanvasServiceMock }],
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(PopDialogValidateGameComponent);
+        dialogSpy = spyOn(TestBed.inject(MatDialog), 'open').and.returnValue(dialogRefSpyObj);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create a pop modal', () => {
-    component.onCreateCreateGame()
-    expect(dialogSpy).toHaveBeenCalled()
-  
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-  it('should change the value of the radius', () => {
-    const mockValue = 3;
-    component.onRadiusChanged(mockValue);
-    expect(component.valueChosen).toEqual(mockValue) 
-  });
+    it('should create a pop modal', () => {
+        component.onCreateCreateGame();
+        expect(dialogSpy).toHaveBeenCalled();
+    });
 
-  it('should change to true if value chosen in not null', () => {
-    const mockValue = 5;
-    component.valueChosen = mockValue; 
-    component.startsGeneratingImageDifferenceAndNumberDifference();
-    expect(component.areImageDifferenceAndNumberDifferenceReady).toBeTruthy()
-  });
+    it('should change the value of the radius', () => {
+        const mockValue = 3;
+        component.onRadiusChanged(mockValue);
+        expect(component.valueChosen).toEqual(mockValue);
+    });
 
+    it('should change to true if value chosen in not null', () => {
+        const mockValue = 5;
+        component.valueChosen = mockValue;
+        component.startsGeneratingImageDifferenceAndNumberDifference();
+        expect(component.areImageDifferenceAndNumberDifferenceReady).toBeTruthy();
+    });
 
-  afterEach(() => {
-    fixture.destroy();
-    TestBed.resetTestingModule();
-  });
-
-
-
-
+    afterEach(() => {
+        fixture.destroy();
+        TestBed.resetTestingModule();
+    });
 });
