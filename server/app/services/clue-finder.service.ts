b2ce9bb5c43d2a06cd743896c16a9c364ca8b0a4
rak553 <raphael62243@gmail.com>
2022-11-12 11:03:48 -0500

    [Feature] Ajout d'une fonction pour trouver la limite de chaque quadrant dans ClueFinderService

--- server/app/services/clue-finder.service.ts
+++ server/app/services/clue-finder.service.ts
@@ -2,6 +2,7 @@ import { FIRST_CLUE_NB, FIRST_CLUE_QUANDRANT_NB, SECOND_CLUE_NB, SECOND_CLUE_QUA
 import { IMAGE_HEIGHT, IMAGE_WIDTH } from '@common/const';
 import { Position } from '@common/position';
 import { randomInt } from 'crypto';
+import { Positions } from 'interfaces/positions';
 import { Service } from 'typedi';
 
 @Service()
@@ -19,11 +20,37 @@ export class ClueFinderService {
     }
 
     private findClueQuadrant(numberOfQuadrants: number, differencesList: number[][]): number {
-        const clueDifferenceNumber = randomInt(differencesList.length);
-        const quadrantsLimits: Position[] = [];
-        const quandrantsWidthDistance = IMAGE_WIDTH / numberOfQuadrants;
-        const quandrantsHeightDistance = IMAGE_HEIGHT / numberOfQuadrants;
+        const clueDifferenceNumber: number = randomInt(differencesList.length);
+        const quadrantsLimits: Positions[] = this.findQuadrantsLimits(numberOfQuadrants);
 
         return 0;
+    }
+
+    private findQuadrantsLimits(numberOfQuadrants: number): Positions[] {
+        const numberOfLimitsTranslations = Math.sqrt(numberOfQuadrants);
+        const quandrantsWidthDistance: number = IMAGE_WIDTH / numberOfLimitsTranslations;
+        const quandrantsHeightDistance: number = IMAGE_HEIGHT / numberOfLimitsTranslations;
+        const quadrantsLimits: Positions[] = [];
+
+        for (let currentWidthPos = 0; currentWidthPos < numberOfLimitsTranslations; currentWidthPos++) {
+            for (let currentHeightPos = 0; currentHeightPos < numberOfLimitsTranslations; currentHeightPos++) {
+                const beginningQuandrantPos: Position = {
+                    x: quandrantsWidthDistance * currentWidthPos,
+                    y: quandrantsHeightDistance * currentHeightPos,
+                };
+
+                const endingQuandrantPos: Position = {
+                    x: quandrantsWidthDistance * (currentWidthPos + 1) - 1,
+                    y: quandrantsHeightDistance * (currentHeightPos + 1) - 1,
+                };
+
+                quadrantsLimits.push({
+                    beginningPosition: beginningQuandrantPos,
+                    endingPosition: endingQuandrantPos,
+                });
+            }
+        }
+
+        return quadrantsLimits;
     }
 }
---
+++ server/interfaces/positions.ts
@@ -0,0 +1,6 @@
+import { Position } from '@common/position';
+
+export interface Positions {
+    beginningPosition: Position;
+    endingPosition: Position;
+}
