4bcda25781d2d72b4fabd717ca505571701c421c
rak553 <raphael62243@gmail.com>
2022-11-13 10:14:51 -0500

    [Feature] Ajout de ClueManagerService

---
+++ server/app/services/clue-manager.service.spec.ts
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import { ClueManagerService } from './clue-manager.service';
+
+describe('ClueManagerService tests', () => {
+    let clueManagerService: ClueManagerService;
+
+    beforeEach(() => {
+        clueManagerService = new ClueManagerService();
+    });
+    afterEach(async () => {
+        sinon.restore();
+    });
+
+    it('should be true', () => {
+        expect(true).to.be.true;
+    });
+});
