abf4f9d9bd2e49d268955469491f24625fe09c5c
BFaras <71102846+BFaras@users.noreply.github.com>
2022-11-23 20:12:33 -0500

    changement du indice

---
+++ client/src/app/components/pop-dialogs/pop-dialog-abandon-verification/pop-dialog-abandon-verification.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pop-dialog-abandon-verification',
+  templateUrl: './pop-dialog-abandon-verification.component.html',
+  styleUrls: ['./pop-dialog-abandon-verification.component.scss']
+})
+export class PopDialogAbandonVerificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
