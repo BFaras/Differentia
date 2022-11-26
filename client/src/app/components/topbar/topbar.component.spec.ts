92f738ff0aba16c706e0d7fa27a3ba206c63d0db
rak553 <raphael62243@gmail.com>
2022-11-26 16:00:28 -0500

    [Test] Test des event de socket dans TopBarComp

--- client/src/app/components/topbar/topbar.component.spec.ts
+++ client/src/app/components/topbar/topbar.component.spec.ts
@@ -31,4 +31,16 @@ describe('TopbarComponent', () => {
         const spy = spyOn(socketService, 'send');
         expect(spy).toHaveBeenCalledWith('get clue for player');
     });
+
+    it('should decrement the amount of clue on a Clue with quadrant of difference event', () => {
+        const expectedAmountOfClues = component.clueAmountLeft - 1;
+        socketTestHelper.emit('Clue with quadrant of difference');
+        expect(component.clueAmountLeft).toEqual(expectedAmountOfClues);
+    });
+
+    it('should set the clue amount left at 0 on a Clue with difference pixels event', () => {
+        const expectedAmountOfClues = 0;
+        socketTestHelper.emit('Clue with difference pixels');
+        expect(component.clueAmountLeft).toEqual(expectedAmountOfClues);
+    });
 });
